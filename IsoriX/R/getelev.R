06dc4fbe72e999d9b568cde064168a78bcc174da
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-05-29 17:28:05 +0200

    new function getprecip

--- IsoriX/R/getelev.R
+++ IsoriX/R/getelev.R
@@ -1,5 +1,5 @@
 #' @rdname IsoriX-defunct
-GetElev <- function (...) {
+GetElev <- function(...) {
   .Defunct("getelev")
 }
 
@@ -42,10 +42,10 @@ GetElev <- function (...) {
 #' ## getelev()
 #' 
 #' @export getelev
-getelev <- function (path = NULL,
-                     overwrite = FALSE,
-                     verbose = interactive()
-                     ) {
+getelev <- function(path = NULL,
+                    overwrite = FALSE,
+                    verbose = interactive()
+                    ) {
 
   ## Define web address and file name
   address.elev <- "http://62.141.164.7/download/gmted2010_30mn.tif"
@@ -67,13 +67,72 @@ getelev <- function (path = NULL,
   return(invisible(NULL))
 }
 
+#' Download an rasters of monthly precipitation from internet
+#' 
+#' The function \code{getprecip} allows for the download of rasters of monthly
+#' precipitation from internet. It downloads the "precipitation (mm) WorldClim
+#' Version2" at a spatial resolution of 30 seconds (~1 km2). After download, the
+#' function also unzip the file. The function
+#' \code{getprecip} uses the generic function \code{downloadfile} that can also be
+#' used to download directly other files.
+#' 
+#' In the argument "path" is not provided, the file will be stored in the 
+#' current working directory. The functions can create new directories, so you 
+#' can also indicate a new path. The integrity of the elevation raster is tested
+#' after a call to \code{getprecip}. In case of corruption, try downloading the
+#' file again, specifying overwrite = TRUE to overwrite the corrupted file.
+#' 
+#' @aliases getprecip downloadfile
+#' @inheritParams getelev
+#' @source \url{http://worldclim.org/version2}
+#' @examples
+#' 
+#' ## To download the monthly precipitation
+#' ## in your current working
+#' ## directory, just type:
+#' ## getprecip()
+#' ## Mind that the file weights ca. 1GB!
+getprecip <- function(path = NULL,
+                      overwrite = FALSE,
+                      verbose = interactive()
+                      ) {
+  
+  ## Define web address and file name
+  address.precip <- "http://biogeo.ucdavis.edu/data/worldclim/v2.0/tif/base/wc2.0_30s_prec.zip"
+  filename.precip <- "wc2.0_30s_prec.zip"
+  
+  ## Define md5sum
+  ## (created with tools::md5sum("wc2.0_30s_prec.zip"))
+  md5sum.elev <- "afd435222a328efb4ab9487a3fe0b6d4"
+  
+  ## Download and check file
+  path.to.zip <- downloadfile(address = address.precip,
+                              filename = filename.precip,
+                              path = path,
+                              overwrite = overwrite,
+                              md5sum = md5sum.elev,
+                              verbose = verbose
+  )
+  
+  ## Unzip the file
+  if (verbose > 0) {
+    print("unzipping in progress...", quote = FALSE)
+  }
+  unzip(path.to.zip, exdir = "wc2.0_30s_prec")
+  
+  if (verbose > 0) {
+    print("unzipping done!", quote = FALSE)
+  }
+  
+  return(invisible(NULL))
+}
 
 ## The following function is a generic function to download files and check 
 ## their binary integrity
 
-downloadfile <- function (address = NULL, filename = NULL, path = NULL,
-                          overwrite = FALSE, md5sum = NULL, verbose = interactive()
-                          ) {
+downloadfile <- function(address = NULL, filename = NULL, path = NULL,
+                         overwrite = FALSE, md5sum = NULL, verbose = interactive()
+                        ) {
   
   if (verbose > 0) {
     print(paste("the function attempts to download", filename, "from internet"))
@@ -114,7 +173,7 @@ downloadfile <- function (address = NULL, filename = NULL, path = NULL,
   ## Checking MD5sum
   if (!is.null(md5sum)) {
     if (requireNamespace("tools", quietly = TRUE)) {
-      if(tools::md5sum(complete.path) == md5sum) {
+      if (tools::md5sum(complete.path) == md5sum) {
         print("the file seems OK (md5sums do match)")
       } else {
         warning("the file seems to be corructed (md5sums do not match)")
