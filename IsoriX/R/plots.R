2f5b20180a4053dc6709030ccc16345174e33491
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-06-16 18:35:40 +0200

    palette system compeltly revised

--- IsoriX/R/plots.R
+++ IsoriX/R/plots.R
@@ -25,24 +25,37 @@
 #' When called upon an object of class \code{isorix}, the plot function draws a
 #' fine-tuned plot of the assignment. You can use the argument "who" to choose
 #' between plotting the assignment for the group or for some individuals (check
-#' the vignette "Workflow" for examples). The arguments "cutoff", "sources",
-#' "calib", "borders", "mask" and "mask2" are used to fine-tune additional
-#' layers that can be added to the main plot to embellish it. These arguments
-#' must be lists that provide details on how to draw, respectively, the area
-#' outside the prediction interval (for assignment plots), the locations of
-#' sources (for both isoscape and assignment plots), the locations of the
-#' calibration sampling area (for assignment plots, the borders (for both types
-#' of plots) and a mask (again, for both)). For assignment maps, an extra mask
-#' can be used (mask2), as one may want to add a mask covering the area outside
-#' the biological range of the species. Within these lists, the element "lwd",
-#' "col", "cex", "pch" and "fill" influences their respective objects as in
-#' traditional R plotting functions (see \code{\link{par}} for details). The 
-#' element "draw" should be a \var{logical} that indicates whether the layer 
-#' must be created or not. The argument "borders" (within the list borders) 
-#' expects an object of the class \var{SpatialPolygons} such as the object 
-#' "countries" provided with this package. The argument "mask" (within the list 
-#' maks) expects an object of the class \var{SpatialPolygons} such as the object
-#' oceanmask provided with this package (see examples).
+#' the vignette "Workflow" for examples).
+#' 
+#' The arguments "cutoff", "sources", "calib", "borders", "mask", and "mask2"
+#' are used to fine-tune additional layers that can be added to the main plot to
+#' embellish it. These arguments must be lists that provide details on how to
+#' draw, respectively, the area outside the prediction interval (for assignment
+#' plots), the locations of sources (for both isoscape and assignment plots),
+#' the locations of the calibration sampling area (for assignment plots, the
+#' borders (for both types of plots), and the mask (again, for both)). For 
+#' assignment maps, an extra mask can be used (mask2), as one may want to add a 
+#' mask covering the area outside the biological range of the species. Within 
+#' these lists, the element "lwd", "col", "cex", "pch" and "fill" influences 
+#' their respective objects as in traditional R plotting functions (see 
+#' \code{\link{par}} for details). The element "draw" should be a \var{logical} 
+#' that indicates whether the layer must be created or not. The argument 
+#' "borders" (within the list borders) expects an object of the class 
+#' \var{SpatialPolygons} such as the object "countries" provided with this 
+#' package. The argument "mask" (within the list maks) expects an object of the 
+#' class \var{SpatialPolygons} such as the object oceanmask provided with this 
+#' package (see examples).
+#' 
+#' The argument "palette" is used to define how to colour the isoscape and 
+#' assignment plot. Within this list, "fn" is used to specify the function that 
+#' is used to sample the colours. "step" defines the number of units on the 
+#' z-scale that shares a given colour and "range" can be used to constrain the 
+#' minimum and/or maximum values to be drawn (e.g. range = c(0, 1)). This latter
+#' argument is usefull if one wants to create several plots with the same 
+#' z-scale. The argument "n.labels" allows for the user to approximatively 
+#' define the maximum number of numbers plotted on the z-scale. The argument 
+#' "digits" defines the number of digits displayed for the numbers used as
+#' labels.
 #' 
 #' @name plots
 #' @aliases plot.isofit plot.isoscape plot.calibfit plot.isorix plot
@@ -70,7 +83,8 @@
 #'   (e.g. an ocean mask) (see details)
 #' @param mask2 A \var{list} containing information for the display of a mask
 #'   (e.g. a distribution mask) (see details)
-#' @param palette An optional vector of colours (\var{character})
+#' @param palette A \var{list} containing information for the display of the 
+#'   colours for the isoscape (see details)
 #' @param plot A \var{logical} indicating whether the plot shall be plotted or
 #'   just returned
 #' @param ... Additional arguments (not in use)
@@ -95,9 +109,9 @@ NULL
 plot.isoscape <- function(x,
                           which   = "mean",
                           sources = list(draw = TRUE, cex = 0.5, pch = 2, lwd = 1, col = "red"),
-                          borders  = list(borders = NULL, lwd = 0.5, col = "black"),
+                          borders = list(borders = NULL, lwd = 0.5, col = "black"),
                           mask    = list(mask = NULL, lwd = 0, col = "black", fill = "black"),
-                          palette = grDevices::terrain.colors(20),
+                          palette = list(fn = grDevices::colorRampPalette(IsoriX::isopalette1, bias = 0.5), step = NA, range = c(NA, NA), n.labels = 11, digits = 2),
                           plot    = TRUE,
                           ... ## we cannot remove the dots because of the S3 export...
                           ) {
@@ -122,14 +136,19 @@ plot.isoscape <- function(x,
       }
     }
     
-    if (("isofit" %in% class(x)) & !(which %in% c("mean",
-                                                  "mean.predVar", "mean.residVar", "mean.respVar",
-                                                  "disp", "disp.predVar", "disp.residVar", "disp.respVar"
-                                                  )
-                                     )
-        ) {
+    if (("isofit" %in% class(x)) & 
+        !(which %in% c("mean", "mean.predVar", "mean.residVar", "mean.respVar",
+                       "disp", "disp.predVar", "disp.residVar", "disp.respVar"))) {
       stop("argument 'which' unknown")
     }
+    
+    ## compute the colors
+    colours <- .cutandcolor(var     = x$isoscape[[which]]@data@values,
+                           step     = palette$step,
+                           range    = palette$range,
+                           palette  = palette$fn,
+                           n.labels = palette$n.labels,
+                           digits   = palette$digits)
     
     ## define y.title
     simu.title <- ""
@@ -142,18 +161,17 @@ plot.isoscape <- function(x,
     map <- rasterVis::levelplot(x$isoscape[[which]],
                                 maxpixels = 4e6,
                                 margin = FALSE,
-                                cuts = length(palette) - 1,
-                                col.regions = palette,
-                                main = bquote(.(simu.title)~.(sub(".", " ", which, fixed = TRUE))~delta*D[p])
-                                )
+                                col.regions = colours$all.cols,
+                                at = colours$at,
+                                colorkey = list(labels = list(at = colours$at.keys, labels = colours$at.labels)),
+                                main = bquote(.(simu.title)~.(sub(".", " ", which, fixed = TRUE))~delta*D[p]))
     
     ## create the additional plot(s)
     decor <- .BuildAdditionalLayers(x = x,
                                     sources = sources,
                                     calib = NULL,
                                     borders = borders,
-                                    mask = mask
-                                    )
+                                    mask = mask)
     
     
     complete.map <- map + decor$borders.layer + decor$mask.layer + decor$sources.layer
@@ -176,14 +194,14 @@ plot.isoscape <- function(x,
 plot.isorix <- function(x,
                         who     = "group",
                         what    = "pv",
-                        cutoff   = list(draw = TRUE, level = 0.05, col = "#909090"),
-                        sources  = list(draw = TRUE, cex = 0.5, pch = 2, lwd = 1, col = "red"),
-                        calib    = list(draw = TRUE, cex = 0.5, pch = 4, lwd = 1, col = "blue"),
+                        cutoff  = list(draw = TRUE, level = 0.05, col = "#909090"),
+                        sources = list(draw = TRUE, cex = 0.5, pch = 2, lwd = 1, col = "red"),
+                        calib   = list(draw = TRUE, cex = 0.5, pch = 4, lwd = 1, col = "blue"),
                         borders = list(borders = NULL, lwd = 0.5, col = "black"),
                         mask    = list(mask = NULL, lwd = 0, col = "black", fill = "black"),
                         mask2   = list(mask = NULL, lwd = 0, col = "purple", fill = "purple"),
-                        palette = rev(grDevices::terrain.colors(100)),
-                        plot     = TRUE,
+                        palette = list(fn = grDevices::colorRampPalette(IsoriX::isopalette2, bias = 0.5), step = NA, range = c(0, 1), n.labels = 11, digits = 2),
+                        plot    = TRUE,
                         ... ## we cannot remove the dots because of the S3 export...
                         ) {
 
@@ -195,25 +213,28 @@ plot.isorix <- function(x,
     stop("wrong palette, more colors needed")  
   }
   
-  ## adding colour for non assignment
-  if (cutoff$level > 0) {
-    palette <- c(cutoff$col, palette)
-  }
-  
   ## changing cutoff level to null when we don't want to draw the cutoff
   if (what != "pv" | !cutoff$draw) {
     cutoff$level <- 0
   }
   
   ## create the main plot(s)
-  splits <- seq(0, 1, length = length(palette))
-
   if ("group" %in% who) {
-    map <- rasterVis::levelplot(x$group$pv * (x$group$pv > cutoff$level),
+    colours <- .cutandcolor(var        = x$group$pv@data@values,
+                            step       = palette$step,
+                            range      = palette$range,
+                            palette    = palette$fn,
+                            cutoff     = cutoff$level,
+                            col.cutoff = cutoff$col,
+                            n.labels   = palette$n.labels,
+                            digits     = palette$digits)
+    
+    map <- rasterVis::levelplot(x$group$pv, # x$group$pv * (x$group$pv > cutoff$level)
                                 maxpixels = 4e6,
                                 margin = FALSE,
-                                at = splits,
-                                col.regions = palette,
+                                col.regions = colours$all.cols,
+                                at = colours$at,
+                                colorkey = list(labels = list(at = colours$at.keys, labels = colours$at.labels)),
                                 main = "Group assignment"
                                 )
   } else {
@@ -222,13 +243,23 @@ plot.isorix <- function(x,
     } else {
       NULL
     }
-    map <- rasterVis::levelplot(x$indiv[[what]][[who]] * (x$indiv$pv[[who]] > cutoff$level),
+    
+    colours <- .cutandcolor(var        = x$indiv[[what]][[who]]@data@values,
+                            step       = palette$step,
+                            range      = palette$range,
+                            palette    = palette$fn,
+                            cutoff     = cutoff$level,
+                            col.cutoff = cutoff$col,
+                            n.labels   = palette$n.labels,
+                            digits     = palette$digits)
+    
+    map <- rasterVis::levelplot(x$indiv[[what]][[who]], #x$indiv[[what]][[who]] * (x$indiv$pv[[who]] > cutoff$level)
                                 maxpixels = 4e6,
                                 margin = FALSE,
-                                at = splits,
-                                col.regions = palette,
-                                main = main.title
-                                )
+                                col.regions = colours$all.cols,
+                                at = colours$at,
+                                colorkey = list(labels = list(at = colours$at.keys, labels = colours$at.labels)),
+                                main = main.title)
   }
     
   ## create the additional plot(s)
@@ -268,25 +299,61 @@ plot.isorix <- function(x,
 }
 
 
-.cutandcolor <- function(var, step, range = NA, palette = viridisLite::viridis) {
-  where.cut <- seq(floor(min(var, na.rm = TRUE)),
-                   ceiling(max(var, na.rm = TRUE)) + step, step)
-  if (!any(is.na(range))) {
-    where.cut <- seq(min(range), max(range), step)
-    if ((min(var, na.rm = TRUE) < min(where.cut)) ||
-        (max(var, na.rm = TRUE) > max(where.cut)))
-      stop(
-        paste0("Range too small! It should be at least: [",
-               floor(min(var, na.rm = TRUE)), "-", ceiling(max(var, na.rm = TRUE)),
-               "]")
-      )
+.cutandcolor <- function(var,
+                         step = NA,
+                         range = NA,
+                         palette = viridisLite::viridis,
+                         cutoff = NA,
+                         col.cutoff = "#909090",
+                         n.labels = 99,
+                         digits = 2) {
+  if (is.na(n.labels)) {
+    warning("The argument n.labels of the palette was changed to 10 because it was not defined!")
+    n.labels <- 10
+  }
+  if (length(range) == 1) {
+    range <- c(NA, NA)
+  }
+  if (is.na(range)[1]) {
+    range[1] <- min(var, na.rm = TRUE)
+  }
+  if (is.na(range)[2]) {
+    hard.top <- FALSE
+    range[2] <- max(var, na.rm = TRUE)
+  } else {
+    hard.top <- TRUE
+  }
+  if (is.na(step)) {
+    step <- (max(range, na.rm = TRUE) - min(range, na.rm = TRUE)) / (n.labels - 1)
+  }
+  where.cut <- seq(min(range, na.rm = TRUE), (max(range, na.rm = TRUE)), step)
+  if ((max(where.cut) < max(var, na.rm = TRUE)) & !hard.top) {
+    where.cut <- c(where.cut, max(where.cut) + step)
+    n.labels <- n.labels + 1
+  }
+  if ((min(var, na.rm = TRUE) < min(where.cut)) || (max(var, na.rm = TRUE) > max(where.cut))) {
+    warning(paste0("Range for palette too small! It should be at least: [",
+                   min(var, na.rm = TRUE), "-", max(var, na.rm = TRUE), "]"))
+  }
+  if (!is.na(cutoff)) {
+    where.cut <- sort(unique(c(cutoff, where.cut)))
   }
   cats <- cut(var, where.cut, ordered_result = TRUE)
   all.cols <- do.call(palette, list(n = length(levels(cats))))
+  if (!is.na(cutoff)) {
+    all.cols[1:(which(where.cut == cutoff) - 1)] <- col.cutoff
+  }
   cols <- all.cols[match(cats, levels(cats))]
-  return(list(
-    cols = cols,
-    at = where.cut,
-    all.cols = all.cols
-  ))
+  at.keys <- where.cut
+  if (length(at.keys) > n.labels) {
+    at.keys <- seq(min(where.cut), max(where.cut), length = n.labels)
+    if (!is.na(cutoff)) {
+      at.keys <- sort(unique(c(cutoff, at.keys)))
+    }
+  }
+  if (sum(at.keys %% 1) == 0) {
+    digits <- 0
+  }
+  at.labels <-  formatC(round(at.keys, digits = digits), digits = digits, format = "f")
+  return(list(cols = cols, at = where.cut, all.cols = all.cols, at.keys = at.keys, at.labels = at.labels))
 }
