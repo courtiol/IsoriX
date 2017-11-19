7c76a093c7946de2e86858232d521fb644cbddf6
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-11-19 15:46:45 +0100

    add testthat

---
+++ IsoriX/tests/testthat/test_internals.R
@@ -0,0 +1,20 @@
+test_that("print_nice_and_round", {
+  expect_equal(.print_nice_and_round(1.222), "1.22")
+  expect_equal(.print_nice_and_round(1.226), "1.23")
+  expect_equal(.print_nice_and_round(0.0, digits = 1), "0.0")
+  expect_equal(.print_nice_and_round(0.0, digits = 2), "0.00")
+  expect_error(.print_nice_and_round(0.0, digits = -2))
+  }
+)
+
+test_that("make rasters and spatial points", {
+  expect_equal(class(.create_raster(long = seq_len(100)/10, lat = seq_len(100)/10, values = runif(10000)))[[1]], "RasterLayer")
+  expect_equal(class(.create_spatial_points(long = seq_len(100)/10, lat = seq_len(100)/10, values = runif(100)))[[1]], "SpatialPointsDataFrame")
+  }
+)
+
+test_that("convert month to numbers", {
+  expect_equivalent(.converts_months_to_numbers("Jan"), 1)
+  expect_warning(.converts_months_to_numbers("Jo"))
+  }
+)
---
+++ IsoriX/tests/testthat/test_options.R
@@ -0,0 +1,9 @@
+test_that("test options modification and retrieval", {
+  options_IsoriX(example_maxtime = 30)
+  expect_equal(getOption_IsoriX("example_maxtime"), 30)
+  expect_equal(getOption_IsoriX("Ncpu"), 2)
+  expect_equal(length(getOption_IsoriX()), 3)
+  expect_error(getOption_IsoriX("bidon"))
+  expect_error(getOption_IsoriX(c("example_maxtime", "bidon")))
+  }
+)
