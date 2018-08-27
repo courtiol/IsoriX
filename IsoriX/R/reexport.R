cae5757b10c06619e4c3451e2b8bdf4bf7366e17
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-08-27 17:32:34 +0200

    reexport useful functions to avoid loading external packages

---
+++ IsoriX/R/reexport.R
@@ -0,0 +1,16 @@
+# reexport from raster:
+
+#' @importFrom raster raster
+#' @export
+raster::raster
+
+# reexport from grid:
+
+#' @importFrom grid grid.text
+#' @export
+grid::grid.text
+
+# reexport from lattice:
+
+# reexport from spaMM:
+
