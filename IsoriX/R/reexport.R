b4a7e66f0cff7957f96489231420d3a173e81e52
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-08-28 17:59:47 +0200

    reexport most function needs to run examples

--- IsoriX/R/reexport.R
+++ IsoriX/R/reexport.R
@@ -1,8 +1,42 @@
+# reexport from sp:
+
+#' @importFrom sp sp.polygons
+#' @export
+sp::sp.polygons
+
+#' @importFrom sp sp.points
+#' @export
+sp::sp.points
+
+
+
 # reexport from raster:
 
 #' @importFrom raster raster
 #' @export
 raster::raster
+
+#' @importFrom raster values
+#' @export
+raster::values
+
+#' @importFrom raster area
+#' @export
+raster::area
+
+
+
+# reexport from rasterVis:
+
+#' @importFrom rasterVis RdBuTheme
+#' @export
+rasterVis::RdBuTheme
+
+#' @importFrom rasterVis levelplot
+#' @export
+rasterVis::levelplot
+
+
 
 # reexport from grid:
 
@@ -10,11 +44,27 @@ raster::raster
 #' @export
 grid::grid.text
 
+
+
 # reexport from lattice:
 
 #' @importFrom lattice xyplot
 #' @export
 lattice::xyplot
+
+#' @importFrom lattice panel.points
+#' @export
+lattice::panel.points
+
+
+
+# reexport from latticeExtra:
+
+#' @importFrom latticeExtra layer
+#' @export
+latticeExtra::layer
+
+
 
 # reexport from spaMM:
 
