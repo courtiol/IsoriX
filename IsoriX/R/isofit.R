728748a9523ee9783a22fb376df9454d36141249
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-08-28 18:01:10 +0200

    add ref + examples using spaMM

--- IsoriX/R/isofit.R
+++ IsoriX/R/isofit.R
@@ -6,7 +6,7 @@
 #' variance in a spatially explicit manner.
 #'
 #' The detailed statistical definition of the isoscape model is described in
-#' Courtiol & Rousset 2017.
+#' Courtiol & Rousset 2017 and summarized in Courtiol et al. 2019.
 #'
 #' Briefly, the fitting procedure of the isoscape model is divided into two
 #' fits: \code{mean_fit} and \code{disp_fit}. \code{mean_fit} corresponds to the
@@ -170,13 +170,17 @@
 #'
 #' @references Courtiol, A., Rousset, F. (2017). Modelling isoscapes using mixed
 #'   models. \url{https://www.biorxiv.org/content/early/2017/10/23/207662}
+#'   
+#' Courtiol A, Rousset F, Rohwäder M, Soto DX, Lehnert L, Voigt CC, Hobson KA, Wassenaar LI, Kramer-Schadt S (2019). Isoscape
+#' computation and inference of spatial origins with mixed models using the R package IsoriX. In Hobson KA, Wassenaar LI (eds.),
+#' Tracking Animal Migration with Stable Isotopes, second edition. Academic Press, London.
 #'
-#'   Rousset, F., Ferdy, J. B. (2014). Testing environmental and genetic effects
-#'   in the presence of spatial autocorrelation. Ecography, 37(8):781-790.
+#'  Rousset, F., Ferdy, J. B. (2014). Testing environmental and genetic effects
+#'  in the presence of spatial autocorrelation. Ecography, 37(8):781-790.
 #'
-#'   Bowen, G. J., Wassenaar, L. I., Hobson, K. A. (2005). Global application of
-#'   stable hydrogen and oxygen isotopes to wildlife forensics. Oecologia,
-#'   143(3):337-348.
+#'  Bowen, G. J., Wassenaar, L. I., Hobson, K. A. (2005). Global application of
+#'  stable hydrogen and oxygen isotopes to wildlife forensics. Oecologia,
+#'  143(3):337-348.
 #' @source \url{http://kimura.univ-montp2.fr/~rousset/spaMM.htm}
 #' @keywords models regression
 #' @examples
@@ -197,6 +201,11 @@
 #' 
 #' ## Diagnostics for the fits
 #' plot(GermanFit)
+#' 
+#' ## Exploration of the fitted models
+#' GermanFit$mean_fit
+#' GermanFit$disp_fit
+#' AIC(GermanFit$disp_fit)
 #' 
 #' }
 #' 
