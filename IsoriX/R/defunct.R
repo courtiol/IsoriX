dd7370c95906575f48d5ca0d35c65d1b88e336dd
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-05-26 09:41:53 +0200

    Fix typo

--- IsoriX/R/defunct.R
+++ IsoriX/R/defunct.R
@@ -1,10 +1,10 @@
-#' Defunct and depreciated functions
+#' Defunct and deprecated functions
 #' 
 #' The function you asked help for has been defunct (i.e. it does not longer
-#' exists) or depreciated (i.e. it will disappear soon). A new function with a
+#' exists) or deprecated (i.e. it will disappear soon). A new function with a
 #' different name is surely doing the old job.
 #' 
-#' @param ... The call of the defunct or depreciated function
+#' @param ... The call of the defunct or deprecated function
 #' @aliases Calibfit GetElev Isofit Isorix Isoscape Isosim QueryGNIP queryGNIP prepdata prepelev prepiso RElevate relevate
 #' @name IsoriX-defunct
 NULL
