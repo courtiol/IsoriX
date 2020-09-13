3fd38abb2a8547ca18fb9e967788da2a52efa6f6
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-09-13 21:04:45 +0200

    Remove non appropriate test

--- IsoriX/tests/testthat/test_spatial_polygons.R
+++
@@ -1,7 +0,0 @@
-test_that("CountryBorders contains a projection", {
-  expect_true(!is.na(slot(CountryBorders, "projargs")))
-})
-
-test_that("OceanMask contains a projection", {
-  expect_true(is.na(slot(OceanMask, "projargs")))
-})
